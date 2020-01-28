14ad55023510940c3a0c3f2794fb272073c602ee
unknown <martinaoyhenard@gmail.com>
2020-01-28 09:48:40 +0100

    6sec try

--- background.js
+++ background.js
@@ -1,6 +1,6 @@
 let imageArray = ['bcn.png','concert.png', 'etd.png', 'levitat.png', 'subarctic.png', 'wave.png'],
 base = './crossfadewebsite/pngs/',
-secs = 5;
+secs = 6;
 
 imageArray.forEach(function(img){
     new Image().src = base + img;
@@ -12,8 +12,8 @@ function crossfade() {
     let k = 0;
     for (i=0; i <imageArray.length; i++) {
         setTimeout(function() {
-            document.getElementById('main-text').style.background ='url(' + base +imageArray[k] + ') no-repeat center center fixed';
-            document.getElementById('main-text').style.backdroundSize ='cover';
+            document.getElementById('main-text').style.backgroundImage ='url(' + base +imageArray[k] + ')';
+            document.getElementById('main-text').style.backgroundSize ='cover';
             if ((k +1) === imageArray.length) {setTimeout(function() {
                 crossfade()}, (secs *1000))} else {k++;}
         }, (secs * 1000) * i)
